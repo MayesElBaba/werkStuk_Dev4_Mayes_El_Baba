78ab6146d488b7ecbc21357e2ef8d16702c70041
Mayes El Baba <mayes.el.baba@student.ehb.be>
2020-05-13 20:18:51 +0200

    HTML&CSS AF

---
+++ js/index.js
@@ -0,0 +1,9 @@
+"use strict"
+console.log('JS Oke');
+$("#over").hide();
+$("#klikOver").click(function () {
+    $("#voorstellingen").hide();
+    $("#form").hide();
+    $("#imgheader").hide();
+    $("#over").show();
+});
